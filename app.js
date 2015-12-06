7dc7ea99f9e82b4366f8086ca6c3081e8ccb8b52
codewreaker <israel.agyeman.prempeh@gmail.com>
2015-12-06 03:05:02 +0000

    Added Async calls and completed the other functions

--- app.js
+++ app.js
@@ -14,7 +14,61 @@ function onLoad() {
 
 
 var deviceReady = $(function () {
-    var fetchJSON;
+    var fetchJSON
+
+
+
+    //An Ajax call to fetch data from server
+    fetchJSON = function () {
+        $obj = JSON.parse(window.localStorage.getItem('credentials'));
+        $str = "opt=4&user_id=" + $obj.user_id;
+        $.when(
+            // Part a populates listSection
+            sendRequest($str, function (data) {
+                $obj = $.parseJSON(data);
+                var $toastContent = $obj.message;
+                Materialize.toast($toastContent, 3000);
+                var data = $obj.data;
+                var top = '<li class="collection-header"><h4>Events</h4></li>';
+                var mid = "";
+                for (var i = 0; i < data.length; i++) {
+                    mid = mid + '<li class="collection-item avatar"><a class="lighten-2 view-product" href="#modal-view" id="' + data[i].event_id + '"><img src="data:image/jpeg;base64,' + data[i].event_picture + '" alt="" class="circle"/></a><span class="title">' + data[i].event_name + '</span><p>' + data[i].event_desc + '</p><span class="controls secondary-content"><a class="btn-floating teal lighten-2 delete-event" id="' + data[i].event_id + '"><i class="fa fa-2x fa-trash"></i></a></span></li>';
+                }
+                $("#listSection").html(top + mid);
+                list_control();
+            }),
+
+            // Part B populates listsection2
+            sendRequest("opt=2", function (data) {
+                $obj = $.parseJSON(data);
+                var $toastContent = $obj.message;
+                Materialize.toast($toastContent, 3000);
+                var data = $obj.data;
+                var top = '<li class="collection-header"><h4>Friends</h4></li>';
+                var mid = "";
+                for (var i = 0; i < data.length; i++) {
+                    mid = mid + '<li class="collection-item avatar"><img src="src/img/logo.png" alt="" class="circle"><span class="title">' + data[i].email + '</span><p>' + data[i].user_telephone + '&nbsp </p><span class="controls secondary-content"></li>';
+                }
+                $("#listSection2").html(top + mid);
+            }),
+            // Part B populates listsection2
+            sendRequest("opt=3", function (data) {
+                $obj = $.parseJSON(data);
+                // Part C populates listsection3
+                var $toastContent = $obj.message;
+                Materialize.toast($toastContent, 3000);
+                var data = $obj.data;
+                var top = '<li class="collection-header"><h4>Tickets</h4></li>';
+                var mid = "";
+                for (var i = 0; i < data.length; i++) {
+                    mid = mid + '<li class="collection-item avatar"><a class="lighten-2 view-product" href="#modal-view" id="' + data[i].event_id + '"><img src="data:image/jpeg;base64,' + data[i].event_picture + '" alt="" class="circle"/></a><span class="title">' + data[i].event_name + '</span><p>&cent;' + data[i].event_rate + '&nbsp </p><span class="controls secondary-content"><a class="btn-floating teal lighten-2 view-receipt" href="#modal-receipt" id=' + data[i].event_id + '"><img src="src/img/logo.png" alt="" class="circle"><i class="fa fa-eye"></i></a></span></li>';
+                }
+                $("#listSection3").html(top + mid);
+
+            }));
+
+    }
+
 
     /**
      * A function to login
@@ -24,19 +78,32 @@ var deviceReady = $(function () {
             $username = $("#login_email").val();
             $pword = $("#login_pword").val();
             $str = "opt=0&username=" + $username + "&pword=" + $pword;
-            sendRequest($str, function (data) {
-                $obj = $.parseJSON(data);
-                var $toastContent = $obj.message;
-                if ($obj.result == 1) {
-                    Materialize.toast($toastContent, 3000);
-                    window.location.replace("main.html");
-                } else {
-                    Materialize.toast($toastContent, 3000);
-                }
+            var obj = $.ajax({
+                type: "POST",
+                url: "http://localhost/mobile_web/otw-server/OTW.php",
+                //url: "http://cs.ashesi.edu.gh/~csashesi/class2016/prophet-agyeman-prempeh/otw-server/OTW.php", //for web
+                data: $str,
+                async: false,
+                cache: false
             });
-
-
+            $obj = $.parseJSON(obj.responseText);
+            var $toastContent = $obj.message;
+            if ($obj.result == 1) {
+                $json_string = {
+                    "user_id": $obj.user_id,
+                    "email": $obj.email
+                };
+                var localData = JSON.stringify($json_string);
+                window.localStorage.setItem('credentials', localData);
+                Materialize.toast($toastContent, 2000);
+                setTimeout(function () {
+                    window.location.replace("main.html");
+                }, 2000);
+            } else {
+                Materialize.toast($toastContent, 3000);
+            }
         });
+
     }
 
     /**
@@ -49,7 +116,10 @@ var deviceReady = $(function () {
                 var $toastContent = $obj.message;
                 if ($obj.result == 1) {
                     Materialize.toast($toastContent, 3000);
-                    window.location.replace("login.html");
+                    window.localStorage.clear();
+                    setTimeout(function () {
+                        window.location.replace("login.html");
+                    }, 1000);
                 } else {
                     Materialize.toast($toastContent, 3000);
                 }
@@ -92,9 +162,11 @@ var deviceReady = $(function () {
 
     //An ajax call to delete a product from the database
     var deleteEvent = function () {
-        $("ul").on('click', 'li .delete-product', function () {
+        $obj = JSON.parse(window.localStorage.getItem('credentials'));
+        $myid = $obj.user_id;
+        $("ul").on('click', 'li .delete-event', function () {
             var id = $(this).prop("id");
-            var str = 'opt=3&event_id=' + id;
+            var str = 'opt=3&event_id=' + id+'&user_id='+$myid;
             sendRequest(str, function (data) {
                 $obj = $.parseJSON(data);
                 var $toastContent = $obj.message;
@@ -134,63 +206,6 @@ var deviceReady = $(function () {
     }
 
 
-    //An Ajax call to fetch data from server
-    fetchJSON = function () {
-        $.when(
-            // Part a populates listSection
-            sendRequest("opt=1", function (data) {
-                $obj = $.parseJSON(data);
-                var $toastContent = $obj.message;
-                Materialize.toast($toastContent, 3000);
-                var data = $obj.data;
-                var top = '<li class="collection-header"><h4>Events</h4></li>';
-                var mid = "";
-                for (var i = 0; i < data.length; i++) {
-                    mid = mid + '<li class="collection-item avatar"><a class="lighten-2 view-product" href="#modal-view" id="' + data[i].event_id + '"><img src="data:image/jpeg;base64,' + data[i].event_picture + '" alt="" class="circle"/></a><span class="title">' + data[i].event_name + '</span><p>' + data[i].event_desc + '</p><span class="controls secondary-content"><a class="btn-floating teal lighten-2 delete-product" id="' + data[i].event_id + '"><i class="fa fa-2x fa-trash"></i></a></span></li>';
-                }
-                $("#listSection").html(top + mid)
-            }),
-
-            // Part B populates listsection2
-            sendRequest("opt=2", function (data) {
-                $obj = $.parseJSON(data);
-                var $toastContent = $obj.message;
-                Materialize.toast($toastContent, 3000);
-                var data = $obj.data;
-                var top = '<li class="collection-header"><h4>Friends</h4></li>';
-                var mid = "";
-                for (var i = 0; i < data.length; i++) {
-                    mid = mid + '<li class="collection-item avatar"><img src="src/img/logo.png" alt="" class="circle"><span class="title">' + data[i].email + '</span><p>' + data[i].user_telephone + '&nbsp </p><span class="controls secondary-content"></li>';
-                }
-                $("#listSection2").html(top + mid);
-            }),
-            // Part B populates listsection2
-            sendRequest("opt=3", function (data) {
-                $obj = $.parseJSON(data);
-                // Part C populates listsection3
-                var $toastContent = $obj.message;
-                Materialize.toast($toastContent, 3000);
-                var data = $obj.data;
-                var top = '<li class="collection-header"><h4>Tickets</h4></li>';
-                var mid = "";
-                for (var i = 0; i < data.length; i++) {
-                    mid = mid + '<li class="collection-item avatar"><a class="lighten-2 view-product" href="#modal-view" id="' + data[i].event_id + '"><img src="data:image/jpeg;base64,' + data[i].event_picture + '" alt="" class="circle"/></a><span class="title">' + data[i].event_name + '</span><p>&cent;' + data[i].event_rate + '&nbsp </p><span class="controls secondary-content"><a class="btn-floating teal lighten-2 view-receipt" href="#modal-receipt" id=' + data[i].event_id + '"><img src="src/img/logo.png" alt="" class="circle"><i class="fa fa-eye"></i></a></span></li>';
-                }
-                $("#listSection3").html(top + mid);
-
-                list_control(data);
-            }));
-
-
-
-
-
-
-
-
-
-    }
-
 
 
     // The barcode of the product is scanned and form is filled
@@ -215,19 +230,18 @@ var deviceReady = $(function () {
             cordova.plugins.barcodeScanner.scan(
                 //check why its is alerting no such product when there is product
                 function (result) {
-                    barcode_id = result.text;
-                    $obj = sendRequest('opt=6&event_id=' + barcode_id);
-                    if ($obj.result == 0) {
-                        alert("No Such Product with barcode:" + result.text);
-                    } else {
-                        id = $obj.data[0].event_id;
-                        currentQuantity = $obj.data[0].product_quantity;
-                        $("#event_name_2").val($obj.data[0].event_name);
-                        $("#event_rate_2").val($obj.data[0].event_rate);
-                        $("#event_id_2").val($obj.data[0].event_id);
-                    }
-
-
+                    $object = JSON.parse(window.localStorage.getItem('credentials'));
+                    $str = "opt=5&user_id="+ $object.user_id+"&barcode="+result.text;
+                    sendRequest($str, function (data) {
+                        $obj = $.parseJSON(data);
+                        if ($obj.result == 0) {
+                            alert("This event is not yet supported in Off The Wall:" + result.text);
+                        } else {
+                            var $toastContent = $obj.message;
+                            Materialize.toast($toastContent, 3000);
+                        }
+                    });
+                    fetchJSON();
                 },
                 function (error) {
 
@@ -302,7 +316,7 @@ var deviceReady = $(function () {
 
 
     // A series of functions that are bound to each list, an alternative for method delegation
-    var list_control = function (data) {
+    var list_control = function () {
         // this function triggers the modal at the bottom of the screen to add data
         $('.view-product').leanModal({
             ready: function () {
@@ -346,6 +360,8 @@ var deviceReady = $(function () {
     viewDetails();
     //a function that starts a barcode reader and adds the event to your list of events
     addMyEvent();
+    //A function to delete an event
+    deleteEvent();
 
 
 
